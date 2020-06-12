f4df4c6ca1c16b7822dbb9004a1c334f8c5db69f
Md Sajid <sameer.ali1580@gmail.com>
2020-06-12 19:45:41 +0530

    first commit

---
+++ src/components/Results.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Results extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result">
+                <p>{result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default Results;
